2f5c428c28cf869549dc6bb244e8fcad48b744f4
Hanas <hanasoke@gmail.com>
2025-11-25 17:49:29 +0700

    finished UpdateEmployee function in employee_handler.go

--- handlers/employee_handler.go
+++ handlers/employee_handler.go
@@ -129,3 +129,69 @@ func (h *EmployeeHandler) CreateEmployee(c *gin.Context) {
 }
 
 // UpdateEmployee - Update employee by ID 
+func (h *EmployeeHandler) UpdateEmployee(c *gin.Context) {
+	id := c.Param("id")
+
+	var employee models.Employee
+	if err := h.DB.First(&employee, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Employee not found"})
+		return 
+	}
+
+	var req models.EmployeeRequest 
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return 
+	}
+
+	// Validate email 
+	if !utils.IsValidEmail(req.Email) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email format"})
+	}
+
+	// Validate hire date 
+	if !utils.IsValidDate(req.HireDate) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid hire date format. Use YYYY-MM-DD"})
+		return 
+	}
+
+	// Validate status if provided
+	if req.Status != "" && !utils.IsValidStatus(req.Status) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid status"})
+		return 
+	}
+
+	hireDate, _ := time.Parse("2006-01-02", req.HireDate)
+
+	// check if NIK or Email already exists (excluding current employee)
+	var existingEmployee models.Employee 
+	if err := h.DB.Where("(nik = ? OR email = ?) AND id != ?", req.NIK, req.Email, id).First(&existingEmployee).Error; 
+	err == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "NIK or Email already exists"})
+		return 
+	}
+
+	// Update employee
+    updates := models.Employee{
+        NIK        : req.NIK,
+        Name	   : req.Name,
+        Email      : req.Email,
+        Position   : req.Position,
+        Department : req.Department,
+        Salary     : req.Salary,
+        HireDate   : hireDate,
+        Status     : req.Status,
+    }
+
+	if err := h.DB.Model(&employee).Updates(updates).Error; 
+	err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update employee"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Employee updated successfully",
+		"data": employee,
+	})
+
+}
